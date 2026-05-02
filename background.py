97e685e44ac06c880c4218857c0b0527b476332d
nayyykettt <artem0906072@icloud.com>
2026-05-02 14:11:26 +0500

    start working and learning pygame

--- background.py
+++ background.py
@@ -1,1 +1,17 @@
+from config import *
+import pygame
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+screen.fill(BLACK)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(60)
